ab6061a559fde062c2096a5bd73c7c2b5e9fe97b
Nolan Neustaeter <github@noolan.ca>
2013-03-04 16:26:05 -0800

    Moving to new version of laravel

--- application/libraries/schema.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Schema extends Laravel\Database\Schema {
-
-	public static function table_exists($table_name) {
-		$database_name = Config::get('database.connections.' . Config::get('database.default') . '.database');
-		return (bool) DB::only("select case when exists (select * from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA = '" . $database_name . "' and TABLE_NAME = '" . $table_name . "') then 1 else 0 end");
-	}
-}
--- application/views/page.blade.php
+++
@@ -1,8 +0,0 @@
-@layout('bootstrap')
-
-@section('content')
-	<h1>{{ $page->title }}</h1>
-	<div id="content">
-		{{ $page->text }}
-	</div>
-@endsection
